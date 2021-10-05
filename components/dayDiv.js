81b06f3cea2576989190317964b25d4de99d7637
Arie Horowitz <arieworkmail@gmail.com>
2021-10-05 15:58:02 +0300

    tooltip cut fix

--- components/dayDiv.js
+++ components/dayDiv.js
@@ -1,7 +1,23 @@
 import styles from '../styles/Home.module.css'
-import React, { useState } from 'react';
+import React, { useEffect, useState, useRef, useCallback } from 'react';
 
 export default function DayDiv({date, addDate, removeDate, leftMouseIsPressed}) {
+
+    // Fixing tooltip cut on the left side
+    const ref = useRef();
+    const [right, setRight] = useState("100%");
+    const [left, setLeft] = useState("");
+
+    useEffect(() => {
+        const dims = ref.current.getBoundingClientRect();
+        if (dims.x < 0 && dims.right < dims.width) {
+            setLeft("100%");
+            let val = Math.round((dims.right - dims.width));
+            // setRight(val + "px");
+            console.log(`right: ${dims.right}, val: ${val}`);
+        }
+    }, []);
+
     const [isMarked, setIsMarked] = useState(false);
 
     const className = styles.daydiv + " " + styles.tooltip + " ";
@@ -30,7 +46,12 @@ export default function DayDiv({date, addDate, removeDate, leftMouseIsPressed}) 
         <div className={className + (isMarked? styles.daydiv_marked : styles.daydiv_unmarked)}
             onMouseEnter={handleMouseEnter}
             onMouseDown={handleMouseDown}>
-                <span className={styles.tooltiptext}>{(date.toISOString().substring(0,10))}</span>
+                <span
+                    ref={ref}
+                    className={styles.tooltiptext}
+                    style={{right: right, left: left}}>
+                        {(date.toISOString().substring(0,10))}
+                </span>
         </div>
     )
 }
