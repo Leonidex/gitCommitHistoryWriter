f70c3560c3af7fe386fdbfde06281c7da7e3d87a
Arie <arie@arie.software>
2023-08-16 14:41:59 +0300

    fixed string and added commit count range

--- components/resultArea.js
+++ components/resultArea.js
@@ -10,7 +10,7 @@ export default function ResultArea({ selectedDates }) {
         let finalString =
             "# Use this bash code on your terminal while it is on a directory of the git project you would like to commit on.\n";
         finalString += "#!/bin/bash\n";
-        finalString += "dates=(";
+        finalString += "dates=";
 
         if (selectedDates.length > 0) {
             finalString += "('";
@@ -23,18 +23,35 @@ export default function ResultArea({ selectedDates }) {
             }
         }
 
-        finalString += ");\n";
-        finalString += "for i in ${!dates[@]};\n";
-        finalString += "do\n";
-        finalString += "    echo ${dates[i]}\n";
-        finalString += '    echo "0" >> touched\n';
-        finalString += '    export GIT_COMMITTER_DATE="${dates[i]}"\n';
-        finalString += '    export GIT_AUTHOR_DATE="${dates[i]}"\n';
-        finalString += "    echo $(git add .)\n";
-        finalString +=
-            '    echo $(git commit -am "none" --date "${dates[i]}")\n';
-        finalString += "    echo ${dates[i]}\n";
-        finalString += "done";
+        finalString += '#!/bin/bash\n'
+
+        finalString += '# Define color codes\n'
+        finalString += 'GREEN="\\033[1;32m"\n'
+        finalString += 'RED="\\033[1;31m"\n'
+        finalString += 'RESET="\\033[0m"\n'
+
+        finalString += 'min=5  # minimum number of commits\n'
+        finalString += 'max=10 # maximum number of commits\n'
+
+        finalString += 'for date in "${dates[@]}"; do\n'
+        finalString += '    echo "Committing for date: $date"\n'
+
+        finalString += '    # Generate a random number between min and max\n'
+        finalString += '    number_of_commits=$(( RANDOM % (max - min + 1) + min ))\n'
+        finalString += '    \n'
+        finalString += '    for ((i=1; i<=$number_of_commits; i++)); do\n'
+        finalString += '        echo "0" >> touched\n'
+        finalString += '        export GIT_COMMITTER_DATE="$date"\n'
+        finalString += '        export GIT_AUTHOR_DATE="$date"\n'
+        finalString += '        git add .\n'
+        finalString += '        if git commit -am "Commit $i of $number_of_commits for $date" --date "$date"; then\n'
+        finalString += '            echo -e "${GREEN}Commit $i of $number_of_commits successful for $date${RESET}"\n'
+        finalString += '        else\n'
+        finalString += '            echo -e "${RED}Commit $i of $number_of_commits failed for $date${RESET}"\n'
+        finalString += '        fi\n'
+        finalString += '    done\n'
+        finalString += 'done\n'
+
 
         setChosenDatesString(finalString);
     };
